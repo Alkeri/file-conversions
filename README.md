30f0bb70ca491ab19495f4159e0423b61f501896
Julian LaNeve <lanevejulian@gmail.com>
2023-08-26 11:31:59 -0400

    initial commit w csv and json support

---
+++ README.md
@@ -0,0 +1,45 @@
+# File Format Conversion CLI Tool
+
+This is a command-line tool for converting between CSV and JSON file formats using Python and pandas.
+
+## Installation
+
+1. Clone or download this repository.
+
+2. Install the required dependencies (pandas) using the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Run the CLI tool using the following command:
+
+```bash
+python file-conversions.py input_file --output output_file
+```
+
+Replace `input_file` with the path to the input file and `output_file` with the desired path for the output file. The tool will automatically infer the formats based on the file extensions and perform the conversion.
+
+### Examples
+
+1. Convert CSV to JSON:
+
+```bash
+python file-conversions.py data.csv --output data.json
+```
+
+2. Convert JSON to CSV:
+
+```bash
+python file-conversions.py data.json --output data.csv
+```
+
+## Supported Formats
+
+Currently, the tool supports conversion between CSV and JSON formats. You can easily extend it to support additional formats by adding new methods to the `Converter` class in the `file-conversions.py` script.
+
+## Contributing
+
+Feel free to contribute to this project by adding new features, improving existing code, or suggesting enhancements. Fork this repository, make your changes, and submit a pull request.
---
+++ file-conversions.py
@@ -0,0 +1,111 @@
+import argparse
+import pandas as pd
+from typing import Dict, Union
+
+
+class Converter:
+    def __init__(self) -> None:
+        """
+        Converter class for handling format conversions.
+        """
+        self.read_formats = {
+            "csv": self.read_csv,
+            "json": self.read_json
+            # Add more format reading functions here
+        }
+
+        self.write_formats = {
+            "csv": self.write_csv,
+            "json": self.write_json
+            # Add more format writing functions here
+        }
+
+    def read_csv(self, input_path: str) -> pd.DataFrame:
+        """
+        Read data from a CSV file using pandas.
+
+        Args:
+            input_path (str): Path to the input CSV file.
+
+        Returns:
+            pd.DataFrame: Loaded data as a pandas DataFrame.
+        """
+        return pd.read_csv(input_path)
+
+    def read_json(self, input_path: str) -> pd.DataFrame:
+        """
+        Read data from a JSON file using pandas.
+
+        Args:
+            input_path (str): Path to the input JSON file.
+
+        Returns:
+            pd.DataFrame: Loaded data as a pandas DataFrame.
+        """
+        return pd.read_json(input_path)
+
+    def write_csv(self, data: pd.DataFrame, output_path: str) -> None:
+        """
+        Write data to a CSV file using pandas.
+
+        Args:
+            data (pd.DataFrame): Data to be written as a pandas DataFrame.
+            output_path (str): Path to the output CSV file.
+        """
+        data.to_csv(output_path, index=False)
+
+    def write_json(self, data: pd.DataFrame, output_path: str) -> None:
+        """
+        Write data to a JSON file using pandas.
+
+        Args:
+            data (pd.DataFrame): Data to be written as a pandas DataFrame.
+            output_path (str): Path to the output JSON file.
+        """
+        data.to_json(output_path, orient="records", indent=4)
+
+    def convert(self, input_path: str, output_path: str) -> None:
+        """
+        Convert data from one format to another.
+
+        Args:
+            input_path (str): Path to the input file.
+            output_path (str): Path to the output file.
+        """
+        input_extension = input_path.split(".")[-1]
+        output_extension = output_path.split(".")[-1]
+
+        if input_extension == output_extension:
+            print("Input and output formats are the same.")
+            return
+
+        if input_extension not in self.read_formats:
+            print(f"Unsupported input format: {input_extension}")
+            return
+
+        if output_extension not in self.write_formats:
+            print(f"Unsupported output format: {output_extension}")
+            return
+
+        data = self.read_formats[input_extension](input_path)
+        self.write_formats[output_extension](data, output_path)
+        print(f"Conversion from {input_extension} to {output_extension} successful.")
+
+
+def main() -> None:
+    """
+    Main function for the format conversion CLI tool.
+    """
+    parser = argparse.ArgumentParser(
+        description="Convert between CSV and JSON formats."
+    )
+    parser.add_argument("input", help="Input file path")
+    parser.add_argument("--output", required=True, help="Output file path")
+    args = parser.parse_args()
+
+    converter = Converter()
+    converter.convert(args.input, args.output)
+
+
+if __name__ == "__main__":
+    main()
